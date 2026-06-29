a255b8064d06528bcaa19d20845cd536622ceba5
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-06-29 23:07:23 +0530

    objects basics

---
+++ 01_basics/09_objects.js
@@ -0,0 +1,14 @@
+//Objects literals in JavaScript
+let person = {
+    name: "Jagriti",
+    age: 21,
+    isStudent: true
+};
+
+console.log(person);
+console.log(person.name); // Jagriti
+console.log(person.age);  // 21
+
+//Modifying Properties
+person.age = 22;
+console.log(person.age); // 22
