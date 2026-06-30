8cc4b6794713a979534d957075a1bf169a796160
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-06-30 23:00:31 +0530

    callback function

---
+++ 02_functions/03_callbackFunc.js
@@ -0,0 +1,13 @@
+//what is callback function in JavaScript?
+//A callback function is a function that is passed as an argument to another function and is executed after some operation has been completed. It allows for asynchronous programming and helps in handling tasks that take time to complete, such as API calls or reading files.
+
+function greet(name, callback) {
+    console.log("Hello " + name);
+    callback();
+}
+
+function sayBye() {
+    console.log("Radhey Radhey!");
+}
+
+greet("Jagriti", sayBye);
