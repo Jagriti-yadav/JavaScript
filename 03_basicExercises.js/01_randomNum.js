fb5b9d43249a06268b93ff1e499327a077d83aa5
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-07-01 22:14:40 +0530

    randum function

---
+++ 03_basicExercises.js/01_randomNum.js
@@ -0,0 +1,12 @@
+
+//Random integer between 0 and 10 (inclusive)
+var randomNum = Math.floor(Math.random() * 11);
+console.log(randomNum);
+
+//Random integer between -10 and 10 (inclusive)
+randomNum = Math.floor(Math.random() * 21) - 10;
+console.log(randomNum);
+
+//Random integer between 1 and 6 (inclusive)
+let diceNumber = Math.floor(Math.random() * 6) + 1;
+console.log(diceNumber);
