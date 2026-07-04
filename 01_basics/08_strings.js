37b6c8f4ae82a8488c090ad509701a3a681ebab4
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-07-04 23:59:02 +0530

    string methods

--- 01_basics/08_strings.js
+++ 01_basics/08_strings.js
@@ -15,3 +15,21 @@ console.log(text2.includes("World")); // true
 console.log(text2.indexOf("World"));  // 6
 console.log(text2.slice(0, 4)); // Java
 console.log(text.substring(4)); // Script
+
+
+// escape characters
+let text3 = "He said, \"Hello!\"";
+console.log(text3);
+let path = "C:\\Users\\John";
+console.log(path);
+
+
+// Template literals
+let name = "Jagriti";
+let age = 21;
+let greeting = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(greeting);
+let message = `Line 1
+Line 2
+Line 3`;
+console.log(message);
