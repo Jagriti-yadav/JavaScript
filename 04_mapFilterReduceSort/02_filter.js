8539c89017d95d13c9f32985160162801d07565e
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-07-09 00:03:33 +0530

    basics of filter method

--- 04_mapFilterReduceSort/02_filter.js
+++ 04_mapFilterReduceSort/02_filter.js
@@ -7,3 +7,7 @@ brr = arr.filter((num)=>{
   }
 });
 console.log(brr);
+//generate some code to filter out even numbers from an array using filter function
+const numbers = [10, 15, 20, 25, 30];
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers); // Output: [10, 20, 30]
