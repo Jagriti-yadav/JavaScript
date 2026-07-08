5e6e2a2de9da20a11ba075414af12b803435dc3a
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-07-09 00:00:17 +0530

    map basics

--- 04_mapFilterReduceSort/01_map.js
+++ 04_mapFilterReduceSort/01_map.js
@@ -9,3 +9,8 @@ arr = arr.map((num)=>{
   return num*num;
 });
 console.log(arr);
+
+//generate some code to square the numbers in an array using map function
+const numbers = [5, 6, 7, 8];
+const squaredNumbers = numbers.map(num => num * num);
+console.log(squaredNumbers); // Output: [25, 36, 49, 64]
